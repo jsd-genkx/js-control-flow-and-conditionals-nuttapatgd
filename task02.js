04f11375b42ab35791ef798ef809457b51f428da
nuttapatgd <nuttapat.gd@gmail.com>
2025-07-15 11:47:20 +0700

    task02 solution

--- task02.js
+++ task02.js
@@ -5,4 +5,8 @@
 
 const userInput = "42";
 // Try changing to "hello", 10, null, NaN
-console.log(typeof userInput);
+if (typeof userInput === "number" && !isNaN(userInput)) {
+  console.log("It is a number.");
+} else {
+  console.log("It is NOT a number.");
+}
